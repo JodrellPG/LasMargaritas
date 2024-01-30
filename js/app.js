5e11ff4bafeeaaf754f28a82be8a64bc688a2dd9
Jodrell Garcia <jhodrellgpolanco@gmail.com>
2024-01-30 11:36:15 -0400

    Barran de navegacion responsive lista

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const navegadorBotones = document.querySelector("#navegador--botones");
+const abrirMenu = document.querySelector("#abrir-menu");
+const cerrarMenu = document.querySelector("#cerrar-menu");
+const cerrarMenu1 = document.querySelector("#cerrar-menu1");
+
+abrirMenu.addEventListener("click", () =>{
+    navegadorBotones.classList.add("visible");
+});
+
+cerrarMenu.addEventListener("click", () =>{
+    navegadorBotones.classList.remove("visible");
+});
+cerrarMenu1.addEventListener("click", () =>{
+    navegadorBotones.classList.remove("visible");
+});
