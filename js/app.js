451aca1e6f923d9e02f3aef4afc8806e610e2a8d
Jodrell Garcia <jhodrellgpolanco@gmail.com>
2024-02-21 12:23:20 -0400

    Aqui se añadio la funcionalidad del slide en la parte de gallery, se modifico un poco la parte de about us y eso

--- js/app.js
+++ js/app.js
@@ -33,3 +33,83 @@ cerrarMenu1.addEventListener("click", () =>{
     navegadorBotones.classList.remove("visible");
     enableScroll();
 });
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll(".gallery .fotos-item");
+    const popup = document.querySelector(".popup-image");
+    const expandedImg = document.querySelector(".popup-image img");
+    const prevButton = document.querySelector(".popup-image .prev");
+    const nextButton = document.querySelector(".popup-image .next");
+
+    let currentIndex = 0;
+    let startX;
+
+    images.forEach((image, index) => {
+        image.addEventListener("click", () => {
+            currentIndex = index;
+            showImage(index);
+        });
+    });
+
+    popup.addEventListener("mousedown", handleMouseDown);
+    popup.addEventListener("mousemove", handleMouseMove);
+    popup.addEventListener("mouseup", handleMouseUp);
+
+    popup.addEventListener("click", (event) => {
+        if (event.target === popup || event.target.tagName.toLowerCase() === "span") {
+            popup.style.display = "none";
+        }
+    });
+
+    prevButton.addEventListener("click", showPrevImage);
+    nextButton.addEventListener("click", showNextImage);
+
+    function showImage(index) {
+        expandedImg.src = images[index].getAttribute("src");
+        popup.style.display = "flex";
+    }
+
+    function showPrevImage() {
+        if (currentIndex > 0) {
+            currentIndex--;
+            showImage(currentIndex);
+        }
+    }
+
+    function showNextImage() {
+        if (currentIndex < images.length - 1) {
+            currentIndex++;
+            showImage(currentIndex);
+        }
+    }
+
+    function handleMouseDown(event) {
+        startX = event.clientX;
+    }
+
+    function handleMouseMove(event) {
+        if (startX === undefined) return;
+        const currentX = event.clientX;
+        const difference = startX - currentX;
+
+        if (Math.abs(difference) > 50) {
+            if (difference > 0 && currentIndex < images.length - 1) {
+                // Mover a la siguiente imagen
+                currentIndex++;
+                showImage(currentIndex);
+                startX = undefined;
+            } else if (difference < 0 && currentIndex > 0) {
+                // Mover a la imagen anterior
+                currentIndex--;
+                showImage(currentIndex);
+                startX = undefined;
+            }
+        }
+    }
+
+    function handleMouseUp() {
+        startX = undefined;
+    }
+});
